bd719ea1d4266efaaa02b2412b622b522ba41168
RamySobhieh <ramysobhieh@email.com>
2022-11-04 13:30:09 +0200

    env local

--- components/Search.js
+++ components/Search.js
@@ -3,7 +3,7 @@ import Dropdown from "./Dropdown";
 
 function Search({ search, setSearch }) {
   return (
-    <div className="flex flex-row space-x-2 justify-between max-w-[1150px] lg:inline pr-4 lg:pl-0 pl-4 lg:pl-0">
+    <div className="flex flex-row space-x-2 justify-between max-w-[1150px] lg:inline pr-4 pl-4 lg:pl-0">
       <div className="max-w-[1150px] w-full bg-[#1A1A1A] rounded-full overflow-hidden border-2 border-[#333333] p-1.5 px-5 pr-8 flex items-center mr-4">
         <div className="h-4 w-4 rounded-full border-2 flex-shrink-0 animate-pulse" />
         <input
